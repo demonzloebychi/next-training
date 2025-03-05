f886f4c7f6636d8e7891130e10b86c31afb8cf24
nikolas <nikolaislnkv@gmail.com>
2025-03-05 15:20:05 +0300

    fix popup and fix menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -40,52 +40,74 @@ const Header = () => {
 
 
     return (
-        <header className="header">
-            <button onClick={() => setIsOpen(!isOpen)} className={`burgerButton ${isOpen ? 'active' : ''}`} id="burger-button">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-            <nav className={`headerNav ${isOpen ? 'active' : ''}`}>
-                <ul className="menu">
-                    {MENU.map(item => 
-                            <li key={item.url}>
-                                <a href={item.url} className="link">
-                                    {item.name}
-                                </a>
-                            </li>
-                    )}
-                </ul>
-            </nav>
-
-            <div 
-                className={`popup-overlay ${isOpenPopup ? 'active' : ''}`}
-                onClick={() => setIsOpenPopup(false)}
-            >
-                <div className={`popup ${isOpenPopup ? 'active' : ''}`}>
-                    <button 
-                        className="close-popup"
-                        onClick={() => setIsOpenPopup(false)}
-                    >
-                        Закрыть
-                    </button>
-                    <h2 className='title-h2'>Связаться с нами</h2>
-                    <CallBackForm />
-                </div>
-            </div>
-
-
-
-            <button 
-                className={styles.sendMessage}
-                onClick={() => setIsOpenPopup(!isOpenPopup)}
+        <>
             
-            >
-                Отправить заявку
-            </button>
+                <div 
+                    className={`popup-overlay ${isOpenPopup ? 'active' : ''}`}
+                    onClick={() => setIsOpenPopup(false)}
+                >
+                    <div className={`popup ${isOpenPopup ? 'active' : ''}`}>
+                        <button 
+                            className="close-popup"
+                            onClick={() => setIsOpenPopup(false)}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width={30} height={30}>
+                                <defs>
+                                <style>
+                                    {`.cls-1{
+                                    fill:none;
+                                    stroke:#000;
+                                    stroke-linecap:round;
+                                    stroke-linejoin:round;
+                                    stroke-width:2px;
+                                    }`}
+                                </style>
+                                </defs>
+                                <title>Cross</title>
+                                <g id="cross">
+                                <line className="cls-1" x1="7" x2="25" y1="7" y2="25" />
+                                <line className="cls-1" x1="7" x2="25" y1="25" y2="7" />
+                                </g>
+                            </svg>
+                        </button>
+                        <h2 className='title-h2'>Связаться с нами</h2>
+                        <CallBackForm />
+                    </div>
+                </div>
+
+            <header className="header">
+                <button onClick={() => setIsOpen(!isOpen)} className={`burgerButton ${isOpen ? 'active' : ''}`} id="burger-button">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <nav className={`headerNav ${isOpen ? 'active' : ''}`}>
+                    <ul className="menu">
+                        {MENU.map(item => 
+                                <li key={item.url}>
+                                    <a href={item.url} className="link">
+                                        {item.name}
+                                    </a>
+                                </li>
+                        )}
+                    </ul>
+                </nav>
 
 
-        </header>
+
+
+                <button 
+                    className={styles.sendMessage}
+                    onClick={() => setIsOpenPopup(!isOpenPopup)}
+                
+                >
+                    Отправить заявку
+                </button>
+
+
+            </header>
+        </>
+      
 
      
     );
