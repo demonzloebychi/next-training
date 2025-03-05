7ad5cc6d1cab2dac340effc85dafe6a1b080e7dc
nikolas <nikolaislnkv@gmail.com>
2025-03-05 11:01:17 +0300

    add input phone mask

--- src/components/CallBackForm/CallBackForm.tsx
+++ src/components/CallBackForm/CallBackForm.tsx
@@ -13,19 +13,28 @@ const CallBackForm = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
 
+  // Функция для обработки ввода с маской
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let input = e.target.value.replace(/\D/g, ''); // Убираем все нецифровые символы
+    if (input.length > 11) input = input.slice(0, 11); // Ограничиваем длину номера
+
+    let formattedPhone = '+7 ';
+    if (input.length > 1) formattedPhone += `(${input.substring(1, 4)}`;
+    if (input.length >= 4) formattedPhone += `) ${input.substring(4, 7)}`;
+    if (input.length >= 7) formattedPhone += `-${input.substring(7, 9)}`;
+    if (input.length >= 9) formattedPhone += `-${input.substring(9, 11)}`;
+
+    setPhone(formattedPhone);
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data: FormData = {
-      name,
-      phone,
-    };
+    const data: FormData = { name, phone };
 
     try {
       const response = await fetch('/api/feedback', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
       });
 
@@ -55,19 +64,16 @@ const CallBackForm = () => {
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          placeholder='Ваше имя'
+          placeholder="Ваше имя"
         />
         <input
           className='input'
-          type="tel" // Исправлено на "tel" для номера телефона
+          type="tel"
           value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          placeholder='Ваш номер телефона'
+          onChange={handlePhoneChange}
+          placeholder="+7 (___) ___-__-__"
         />
-        <button
-          className='button submit-btn'
-          type="submit"
-        >
+        <button className='button submit-btn' type="submit">
           Отправить
         </button>
         {success && <p>Форма отправлена успешно!</p>}
